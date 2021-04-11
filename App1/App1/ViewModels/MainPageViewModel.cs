8e2b5e540418ae712990965e9673e982f6fdfe4c
jamesonatic <jamesonatic@users.noreply.github.com>
2021-04-11 17:48:58 +0200

    MVVM wirings
    Added note logging with the Model-View-ViewModel pattern
    Save Clear the note
    Collecting the notelist

---
+++ App1/App1/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App1.ViewModels
+{
+    class MainPageViewModel: INotifyPropertyChanged
+    {
+
+        public MainPageViewModel()
+        {
+            AllNotes = new ObservableCollection<string>();
+
+            EraseCommand = new Command(() =>
+            {
+                TheNote = string.Empty;
+            });
+
+            SaveCommand = new Command(() =>
+            {
+                AllNotes.Add(theNote);
+
+                TheNote = string.Empty;
+            });
+
+        }
+
+        public ObservableCollection<string> AllNotes { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string theNote;
+
+        public string TheNote
+        {
+            get => theNote;
+            set
+            {
+                theNote = value;
+
+                var args = new PropertyChangedEventArgs(nameof(TheNote));
+
+                PropertyChanged?.Invoke(this, args);
+            }
+        }
+
+        public Command SaveCommand { get; }
+
+        public Command EraseCommand { get; }
+    
+    }
+}
